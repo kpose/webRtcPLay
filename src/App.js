00da5210eb7b7295e79705d4b2cd62a19a5d34dc
kpose <ganihujude@gmail.com>
2021-02-12 12:25:12 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.localVideoref = React.createRef();
+  }
+
+  render() {
+    const constraints = {
+      video: { width: 1280, height: 720 } /* audio: true */,
+    };
+
+    const success = (stream) => {
+      this.localVideoref.current.srcObject = stream;
+    };
+
+    const failure = (e) => {
+      console.log("getUserMedia Error: ", e);
+    };
+
+    navigator.mediaDevices
+      .getUserMedia(constraints)
+      .then(success)
+      .catch(failure);
+    return (
+      <div>
+        <video ref={this.localVideoref} autoPlay></video>
+      </div>
+    );
+  }
 }
-
 export default App;
